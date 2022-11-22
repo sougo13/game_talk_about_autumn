ef0b25c744f9a6fecf567cce2979500f47b64131
radostevdk <radostevdk@silaunion.ru>
2022-11-22 23:12:48 +0700

    настроил webpack

---
+++ game3/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/components/index.tsx",
+  target: "web",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./src/favicon.ico",
+      template:  "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/components/app.css",
+    }),
+  ],
+};
