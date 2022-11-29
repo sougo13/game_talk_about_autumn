820cc4cafb6c68e52f0d54a8c92431100298c5c5
radostevdk <radostevdk@silaunion.ru>
2022-11-29 16:38:04 +0700

    стили

--- game3/src/components/AnimalsCount/index.tsx
+++ game3/src/components/AnimalsCount/index.tsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { animalsCount } from './animalsCountData';
-import AnimalsCountItem from './animalsCountItem';
-import { useNavigate } from 'react-router-dom';
-import './animalsCount.css';
+import React, { useEffect, useState } from "react";
+import { animalsCount } from "./animalsCountData";
+import AnimalsCountItem from "./animalsCountItem";
+import { useNavigate } from "react-router-dom";
+import "./animalsCount.css";
 
 const AnimalsCount: React.FC = () => {
-
   const [count, setCount] = useState<number>(0);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     if (count === animalsCount.length) {
-      navigate('/final');
+      navigate("/final");
     }
-  }, [count])
+  }, [count]);
 
   const inc = () => {
     setCount(count + 1);
-  }
+  };
 
   return (
-    <div className='animals-count-page'>
-      <h1>Посчитай лесных животных — один-два-пять</h1>
-      <div className='animals-count-column'>
-        {
-          animalsCount.map((animal, i) =>
-            <AnimalsCountItem key={i} src={animal.src} count={animal.count} inc={inc} />)
-        }
+    <div className="animals-count-page">
+      <h1 style={{ margin: "0 110px" }}>
+        Посчитай лесных животных — один-два-пять
+      </h1>
+      <div className="animals-count-column">
+        {animalsCount.map((animal, i) => (
+          <AnimalsCountItem
+            key={i}
+            src={animal.src}
+            count={animal.count}
+            inc={inc}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AnimalsCount;
